fbe1205fab193cc293a0de1b64d4915db34de956
friedchiken24 <89746764+friedchiken24@users.noreply.github.com>
2021-09-21 08:56:22 -0400

    Testing commit
    Some garbage

---
+++ Snowballerz - Unity Project/Assets/DummyScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    int number;
+
+    void Start()
+    {
+        number = 5;   
+    }
+}
