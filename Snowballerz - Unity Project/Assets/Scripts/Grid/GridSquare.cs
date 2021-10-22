c9c5e28c0e78cedf299b428723e7d1cde768b65e
friedchiken24 <89746764+friedchiken24@users.noreply.github.com>
2021-10-21 21:07:41 -0400

    Placing towers with scroll wheel ui
    (with bug)

--- Snowballerz - Unity Project/Assets/Scripts/Grid/GridSquare.cs
+++ Snowballerz - Unity Project/Assets/Scripts/Grid/GridSquare.cs
@@ -4,10 +4,6 @@ public class GridSquare : MonoBehaviour, IInteractable
 {
     private GridObject currentObject;
 
-    // temporary tower for testing purposes
-    [SerializeField]
-    PeaShooter peaShooter;
-
     [Tooltip("A prefab to create and place on this tile during Start(). If none, this tile will start empty.")]
     [SerializeField]
     private GridObject initialObject;
@@ -21,32 +17,43 @@ public class GridSquare : MonoBehaviour, IInteractable
 
     private void Start()
     {
-        peaShooter = Resources.Load<PeaShooter>("PeaShooter");
-
         // If a prefab for the initial grid object is defined.
         if ( this.initialObject != null ) 
         {
-            var go = GameObject.Instantiate( initialObject );
+            //var go = GameObject.Instantiate( initialObject );
 
-            Place( go );
+            Place(initialObject);
         }
     }
 
-    public void Interact( Player player )
+    //public void Interact( Player player )
+    //{
+    //    if ( this.currentObject != null )
+    //    {
+    //        this.currentObject.Interact(player);
+    //    }
+    //    else
+    //    {
+    //        peaShooter = Instantiate(peaShooter);
+    //        Place(peaShooter);
+    //    }
+    //}
+
+    public bool Interact(Player player)
     {
-        if ( this.currentObject != null )
+        if (this.currentObject != null)
         {
             this.currentObject.Interact(player);
+            return true;
         }
-        else
-        {
-            peaShooter = Instantiate(peaShooter);
-            Place(peaShooter);
-        }
+
+        return false;
     }
 
     public void Place( GridObject gridObject )
     {
+        gridObject = Instantiate(gridObject);
+
         this.currentObject = gridObject;
 
         gridObject.transform.parent = this.transform;
