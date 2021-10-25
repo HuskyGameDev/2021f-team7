a5decee13723f42a33e47b29eb928babab68cbd2
Eric Belt <ejbelt@mtu.edu>
2021-10-24 20:10:24 -0400

    Timer is working, can count down to zero, to a given value, or infinitley. Prints in time format.

--- Snowballerz - Unity Project/Assets/Scripts/Timer.cs
+++ Snowballerz - Unity Project/Assets/Scripts/Timer.cs
@@ -14,29 +14,70 @@ public class Timer : MonoBehaviour
         TIMER_INCREASE_FOREVER
     }
 
-    public float TimerDuration
+    public float TimerTime
     {
         get;
         set;
     }
 
-    public TimerType Type
-    {
-        get;
-        set;
-    }
+    public TimerType Type;
 
-    public Text _text;
+
+    //Could be efficient and use regex, but no, forget that
+    public Text _text_min;
+    public Text _text_sec;
+    public float TimerDuration;
+
+    private float updateTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Type = TimerType.TIMER_INCREASE;
+        TimerDuration = 70;
+        TimerTime = 0;
+        TimerUpdate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (TimerTime > 0 && Type == TimerType.TIMER_DECREASE || Type == TimerType.TIMER_INCREASE && TimerTime < TimerDuration || Type == TimerType.TIMER_INCREASE_FOREVER)
+        {
+            updateTimer += Time.deltaTime;
+
+            if (updateTimer > 1.0f)
+            {
+                TimerUpdate();
+                updateTimer = updateTimer - 1.0f;
+            }
+        } else
+        {
+            Type = TimerType.TIMER_NULL;
+        }
     }
+
+    void TimerUpdate()
+    {
+        //Just to prove I'm not Yanderedev
+        switch (Type)
+        {
+            case TimerType.TIMER_DECREASE:
+                TimerTime--;
+                break;
+            case TimerType.TIMER_INCREASE_FOREVER:
+                TimerTime++;
+                break;
+            case TimerType.TIMER_INCREASE:
+                TimerTime++;
+                break;
+            default:
+                break;
+        }
+
+        _text_min.text = Mathf.Floor(TimerTime / 60).ToString();
+        _text_sec.text = (TimerTime % 60 < 10) ? "0" + (TimerTime % 60).ToString() : (TimerTime % 60).ToString();
+
+    }
+
 }
