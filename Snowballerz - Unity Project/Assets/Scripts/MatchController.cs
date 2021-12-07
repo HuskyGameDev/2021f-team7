6a08d093d32f182af904b28f6841b89d9bdea2e9
Gabe <goetjens@gmail.com>
2021-12-06 21:05:13 -0500

    Begun work on Animated start-sequence UI

--- Snowballerz - Unity Project/Assets/Scripts/MatchController.cs
+++ Snowballerz - Unity Project/Assets/Scripts/MatchController.cs
@@ -8,7 +8,12 @@ public class MatchController : MonoBehaviour
 {
     [ SerializeField ]
     private MapGrid map;
-    
+
+    [ SerializeField ]
+    private Player p1;
+    [ SerializeField ]
+    private Player p2;
+
     private void Awake()
     {
         
@@ -38,5 +43,29 @@ public class MatchController : MonoBehaviour
             line.P1Flag.OnFlagDeath += flagDeath;
             line.P2Flag.OnFlagDeath += flagDeath;
         }
+
+        // Initialize both players to be disabled.
+        this.p1.enabled = false;
+        this.p2.enabled = false;
+
+        // Flip player 2 to face left.
+        this.p2.GetComponent<Animator>().SetBool("FacingRight", false);
+
+        // TODO: REPLACE THIS WITH A PROPER ANIMATED START SEQUENCE
+        this.StartGame();
+    }
+
+    public void StartGame()
+    {
+        // Enable both players.
+        this.p1.enabled = true;
+        this.p2.enabled = true;
+    }
+
+    public void EndGame()
+    {
+        // Disable both players.
+        this.p1.enabled = false;
+        this.p2.enabled = false;
     }
 }
