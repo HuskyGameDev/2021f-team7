773074bcbf42395bfd3f1babe52439a82a29f6a0
Alex Buiciuc <asbuiciu@mtu.edu>
2021-12-08 17:43:24 -0500

    Started On Title Screen
    Just about done, but it needs keyboard input events

---
+++ Snowballerz - Unity Project/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void ClickOnStartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ClickOnExitButton()
+    {
+
+    }
+}
